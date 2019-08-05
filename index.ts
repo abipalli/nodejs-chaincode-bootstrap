4077f7c39bcccda08f6896590ae8671c9afedf7d
Abinesh Sarvepalli <abisarvepalli@ibm.com>
2019-08-05 15:34:13 -0400

    upd chaincode to typescript with test suite

---
+++ index.ts
@@ -0,0 +1,9 @@
+/*
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+
+import { MyAssetContract } from './src/my-asset/my-asset-contract';
+export { MyAssetContract } from './src/my-asset/my-asset-contract';
+
+export const contracts: any[] = [ MyAssetContract ];
