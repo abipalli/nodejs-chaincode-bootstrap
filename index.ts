f267c56152dfc529804276c31bba6238b1ce0af1
Abinesh Sarvepalli <abisarvepalli@ibm.com>
2019-08-05 16:53:58 -0400

    test deployment of fabcar chaincode

--- index.ts
+++
@@ -1,9 +0,0 @@
-/*
- * SPDX-License-Identifier: Apache-2.0
- */
-
-
-import { MyAssetContract } from './src/my-asset/my-asset-contract';
-export { MyAssetContract } from './src/my-asset/my-asset-contract';
-
-export const contracts: any[] = [ MyAssetContract ];
